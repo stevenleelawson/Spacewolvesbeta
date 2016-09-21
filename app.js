3f5d7c45ec7d42d80c30200faa6794470dee9846
Steven Lawson <unluckygamblermusic@gmail.com>
2016-09-20 20:07:45 -0600

    adding leaderboard

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+var userName;
 $(document).ready(function(){
-
+  $.get("https://spacewolvesserver.herokuapp.com/").then(function (data){
+    for (var i = 0; i < data.length; i++) {
+      console.log(data);
+      $('ul').append('<li>' + data[i]["username"] + " " + data[i]["score"] + '</li>');
+    };
+  });
 });
+
 setTimeout(function(){
   $('.sign').fadeOut("slow");
-  $('.loading').fadeIn("slow");
+  $('.loading').fadeOut("slow");
+  $('.leaderBoard').fadeOut("slow");
 }, 8000);
